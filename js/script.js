ea7e3c703e071a79db2e6bb42e722e0a908f4e92
aquitan <aquitan@mail.ru>
2023-05-02 23:15:38 +0300

    new-article

--- js/script.js
+++ js/script.js
@@ -9,3 +9,40 @@ $('.faq-accordeon .accordeon__question').on('click', function () {
     // и скрыть их при помощи slideUp()
     $('.accordeon .accordeon__answer').not(answer).slideUp();
 });
+$('.new-article-images-mobile').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: `<img src='../../img/slick-arrow-left.svg' class="article-slick-prev article-slick"/>`,
+    nextArrow: `<img src='../../img/slick-arrow-right.svg' class="article-slick-next article-slick"/>`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
